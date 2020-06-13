4f879802242e3833fc74105d3ffe59c83873e096
swarup-fuse <swarup@fusemachines.com>
2020-06-13 13:44:33 +0545

    [FAB] -Added FAB and all required features. -Added Snackbar message whenever more than 4 datas are added to archive list. -Reveal of Archive details on tapping the panel

--- lib/screens/listDetails.dart
+++ lib/screens/listDetails.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+
+import 'package:uitask/helpers/floatingactbutton.dart';
 
 class ListDetails extends StatefulWidget {
   @override
@@ -11,42 +14,56 @@ class ListDetails extends StatefulWidget {
 }
 
 class _ListState extends State<ListDetails> with TickerProviderStateMixin {
-  _showSnackBar(BuildContext context, String s) {
-    final snackBar = SnackBar(content: Text(s));
-    Scaffold.of(context).showSnackBar(snackBar);
+  List<String> allItems;
+  ScrollController scrollController;
+  bool dialVisible = true;
+  List<String> archivedItems;
+  bool visibility = false;
+  var list = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController = ScrollController()
+      ..addListener(() {
+        setDialVisible(scrollController.position.userScrollDirection ==
+            ScrollDirection.forward);
+      });
   }
 
-  List<String> allItems;
-  List<String> archivedItems;
+  void setDialVisible(bool value) {
+    setState(() {
+      dialVisible = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('List Details'),
-        ),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-
-             Container (
-               constraints: BoxConstraints(
-                 maxHeight: 300.0,
-               ),
-               child:getArchiveView(),
-             ),
-
-            Container(
-              child: Text("This is second list"),
-             height: 40.0,
-              margin: EdgeInsets.only(top: 5.0),
+      appBar: AppBar(
+        title: Text('List Details'),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            constraints: BoxConstraints(
+              maxHeight: 300.0,
             ),
-            Expanded(
-              child: getSlideableView(),
-            )
-
-          ],
-        )
+            child: getArchiveView(),
+          ),
+          Container(
+            child: Text("This is second list"),
+            height: 40.0,
+            margin: EdgeInsets.only(top: 5.0),
+          ),
+          Expanded(
+            child: getSlideableView(),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActButton(dialVisible),
     );
   }
 
@@ -76,7 +93,7 @@ class _ListState extends State<ListDetails> with TickerProviderStateMixin {
                 color: Colors.lime,
                 icon: Icons.archive,
                 onTap: () {
-                  updateList(allItems[index]);
+                  updateList(allItems[index], context);
                   print("Add to upper list $index");
                 },
               )
@@ -101,14 +118,40 @@ class _ListState extends State<ListDetails> with TickerProviderStateMixin {
             return Slidable(
               actionPane: SlidableDrawerActionPane(),
               actionExtentRatio: 0.25,
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.lightGreenAccent,
-                  child: Text(archivedItems[index]),
-                  foregroundColor: Colors.white,
+              child: GestureDetector(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.lightGreenAccent,
+                    child: Text(archivedItems[index]),
+                    foregroundColor: Colors.white,
+                  ),
+                  title: Text('List Title: ' + archivedItems[index]),
+                  subtitle: Visibility(
+                      visible: visibility,
+                      child: Column(
+                        children: <Widget>[
+                          Row(children: <Widget>[
+                            Text(
+                              'Swipe Left to Remove',
+                              textAlign: TextAlign.center,
+                            ),
+                          ]),
+                          Row(children: <Widget>[
+                            Text('Sub title 2'),
+                            Padding(
+                              padding: EdgeInsets.only(left: 50.0),
+                              child: Text(
+                                'Description 2',
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          ]),
+                        ],
+                      )),
                 ),
-                title: Text('List Title: ' + archivedItems[index]),
-                subtitle: Text('Swipe Left to Remove'),
+                onTap: () {
+                  setVisibility();
+                },
               ),
               secondaryActions: <Widget>[
                 IconSlideAction(
@@ -128,15 +171,33 @@ class _ListState extends State<ListDetails> with TickerProviderStateMixin {
     }
   }
 
+  void setVisibility() {
+    if (visibility == true) {
+      visibility = false;
+    } else if (visibility == false) {
+      visibility = true;
+    }
+    setState(() {});
+  }
+
+  void createSnackBar(String message, BuildContext context) {
+    final snackBar =
+        new SnackBar(content: new Text(message), backgroundColor: Colors.red);
+
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
   getListElements() {
     var items = List<String>.generate(15, (counter) => "$counter");
     return items;
   }
 
-  void updateList(String index) {
+  void updateList(String index, BuildContext context) {
     int x = int.parse(index);
     if (archivedItems == null) {
       archivedItems = [index];
+    } else if (archivedItems.length > 3) {
+      createSnackBar("Can not add more than four activities", context);
     } else {
       archivedItems.add(index);
     }
